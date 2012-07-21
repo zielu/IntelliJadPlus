0782b440bdf4bf79ab48f0406b3e447b0e5bc274
Lukasz Zielinski <zieluuuu@gmail.com>
2012-07-21 10:30:18 +0200

    120.152 compatibility

--- src/java/net/stevechaloner/intellijad/decompilers/JarExtractor.java
+++ src/java/net/stevechaloner/intellijad/decompilers/JarExtractor.java
@@ -24,18 +24,18 @@ import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
+import com.intellij.openapi.vfs.JarFile;
+import com.intellij.openapi.vfs.JarFile.JarEntry;
 import net.stevechaloner.intellijad.console.ConsoleEntryType;
 
 /**
- * ZipExtractor will pull a file from a given path and extract it into
+ * JarExtractor will pull a file from a given path and extract it into
  * a directory on the file system.
  * 
  * @author Steve Chaloner
  */
-class ZipExtractor
+class JarExtractor
 {
     /**
      * Pattern describing class names, including nested classes.
@@ -46,22 +46,22 @@ class ZipExtractor
      * Extract the given file to the target directory specified in the context.
      *
      * @param context the context of the decompilation operation
-     * @param zipFile the name of the zip file to open
+     * @param jarFile the name of the zip file to open
      * @param packageName the package of the class
      * @param className the name of the class
      * @throws IOException if an error occurs during the operation
      */
     void extract(DecompilationContext context,
-                 ZipFile zipFile,
+                 JarFile jarFile,
                  String packageName,
                  String className) throws IOException
     {
         Pattern p = Pattern.compile(packageName + className + CLASS_PATTERN + ".class");
 
-        Enumeration<? extends ZipEntry> entries = zipFile.entries();
+        Enumeration<? extends JarEntry> entries = jarFile.entries();
         while (entries.hasMoreElements())
         {
-            ZipEntry entry = entries.nextElement();
+            JarEntry entry = entries.nextElement();
             String name = entry.getName();
             Matcher matcher = p.matcher(name);
             if (matcher.matches())
@@ -69,7 +69,7 @@ class ZipExtractor
                 context.getConsoleContext().addMessage(ConsoleEntryType.JAR_OPERATION,
                                                        "message.extracting",
                                                        entry.getName());
-                InputStream inputStream = zipFile.getInputStream(entry);
+                InputStream inputStream = jarFile.getInputStream(entry);
                 int lastIndex = name.lastIndexOf("/");
                 File outputFile = new File(context.getTargetDirectory(),
                                            name.substring(lastIndex));
